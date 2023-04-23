911d28a323ed62c28b848dd003b102d57a651fad
mauboro <trying.again999@outlook.com>
2023-04-23 20:18:58 -0300

    adding the chapter made today

--- land_of_lisp/5-building_a_text_game_engine/engine/main.lisp
+++ land_of_lisp/5-building_a_text_game_engine/engine/main.lisp
@@ -4,3 +4,12 @@
 						   there is a well in front of you.))
 						 (attic (you are in the attic.
 							there is a giant welding a torch in the corner.))))
+
+; (princ (assoc 'garden *nodes*))
+
+(defun describe-location (location nodes)
+  (cadr (assoc location nodes)))
+
+(princ (describe-location 'garden *nodes*))
+(princ (describe-location 'living-room *nodes*))
+
